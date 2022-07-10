1745919580800076570875ff54c7ced15f2f918a
kharon626 <78420479+boost-vladimir112@users.noreply.github.com>
2022-07-11 00:18:27 +0300

    Ragdoll bag version

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,12 +5,14 @@ using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
+    Animator anim;
+
     public int health = 3;
     [SerializeField]
     private Text hpText;
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,12 +20,23 @@ public class Enemy : MonoBehaviour
     {
         if(health <= 0)
         {
-            Destroy(gameObject);
+            anim.enabled = false;
+            Ragdoll();
+            
+            //Destroy(gameObject);
         }
         hpText.text = health.ToString() + "HP";
     }
     public void TakeDamage(int damage)
     {
         health -= damage;
+    }
+    public void Ragdoll()
+    {
+        Rigidbody[] rigidbodies = GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody rigidbody in rigidbodies)
+        {
+            rigidbody.isKinematic = false;
+        }
     }
 }
